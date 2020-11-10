282e8a9c9f0db34ee0adfc48436576378c56f503
DavidCJAdindu <lavishdigitalsolutions@gmail.com>
2020-11-10 23:02:24 +0000

    login/create

--- login.py
+++ login.py
@@ -3,13 +3,30 @@
 '''
 Create a basic login system in your python console. It should be able to ask the person for their username and password or to create an account, so if they dont have an account they can make one. To create an account ask for their email, username and 2 passwords and check if both passwords are the same. Store the information in a text file/database.  
 '''
-
 # ask user if they want to login or create an account
 # ask user for username & password to login
 # to create account ask for email, username, two passwords 
 # validate to see if two password are correct
 # store the data in a text file 
-# add some sort of data to text file for a login
+
+# --------------------------------------------------------------------------------
+
+
+login_ = input("Do you want to login? (Y/N)").lower()
+
+
+ 
+ 
+
+create_ = input("Do you want to create an account?")
+
+
+
+
+
+
+
+
 
 
 
